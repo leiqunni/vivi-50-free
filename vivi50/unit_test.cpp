b4b136070090a97e6bc9ee13a0a66e915b97f57d
vivisuke <vivisuke@3a3fcbb5-9455-4931-b1bf-4cf1dfffbb8a>
2011-04-04 23:47:23 +0000

    git-svn-id: https://svn.osdn.net/svnroot/vivi-50-free/trunk@20 3a3fcbb5-9455-4931-b1bf-4cf1dfffbb8a

---
+++ vivi50/unit_test.cpp
@@ -0,0 +1,82 @@
+//----------------------------------------------------------------------
+//
+//			File:			"unit_test.cpp"
+//			Created:		04-Jun-2009
+//			Author:			津田伸秀
+//			Description:	単体テスト用モジュール
+//
+//----------------------------------------------------------------------
+
+#include	<QString>
+#include	<QDebug>
+#include	"unit_test.h"
+//#include	<iostream>
+
+uint g_total_test_count = 0;
+uint g_total_fail_count = 0;
+
+void ut_output(const QString &mess)
+{
+	qDebug() << mess;
+	//std::cout << mess;
+}
+
+OutputFunc g_ut_output = &ut_output;
+
+void ut_set_output(OutputFunc f)
+{
+	g_ut_output = f;
+}
+
+namespace unit_test {
+	size_t n_test = 0;			//	トータルテスト回数
+	size_t n_failed = 0;		//	トータル失敗回数
+}
+
+//----------------------------------------------------------------------
+
+CUnitTest::CUnitTest(cchar *unitName /*, CVOStream *out*/)
+	: m_unitName(unitName), m_testCount(0), m_failCount(0) /*, m_out(out)*/
+{
+	QString temp(m_unitName);
+	temp += ":\n";
+	g_ut_output(temp);
+}
+
+CUnitTest::~CUnitTest(void)
+{
+	QString temp;
+	if( m_failCount == 0 ) {
+		temp.sprintf("\nOK! (%d tested)\n", m_testCount);
+		g_ut_output(temp);
+	} else {
+		temp.sprintf("\n＊＊＊ NG! (%d failed / %d tested)\n", m_failCount, m_testCount);
+		g_ut_output(temp);
+		for(std::vector<SFailedInfo>::const_iterator itr = m_logs.begin(); itr != m_logs.end(); ++itr) {
+			temp = QString("\"%1\"(%2); %3\n").arg(itr->m_fileName).arg(itr->m_lineNum).arg(itr->m_message);
+			g_ut_output(temp);
+		}
+		g_ut_output("\n");
+	}
+	g_total_test_count += m_testCount;
+	g_total_fail_count += m_failCount;
+}
+
+void CUnitTest::_test(bool b, cchar *fileName, int lineNum)
+{
+#if 1
+	m_testCount += 1;
+	unit_test::n_test += 1;
+	if( b ) {
+		g_ut_output(".");
+	} else {
+		m_failCount += 1;
+		unit_test::n_failed += 1;
+		g_ut_output("F");
+
+		m_logs.push_back(SFailedInfo(fileName, lineNum, "true を期待しましたが false でした"));
+	}
+#endif
+}
+
+
---
+++ vivi50/unit_test.h
@@ -0,0 +1,147 @@
+//----------------------------------------------------------------------
+//
+//			File:			"unit_test.h"
+//			Created:		04-Jun-2009
+//			Author:			津田伸秀
+//			Description:
+//
+//----------------------------------------------------------------------
+
+#pragma once
+
+//#include	<iostream>
+#include	<string>
+#include	<vector>
+#include	<boost/lexical_cast.hpp>
+#include	<QDebug>
+
+typedef const char cchar;
+typedef unsigned int uint;
+
+//typedef void (*OutputFunc)(cchar*);
+typedef void (*OutputFunc)(const QString &);
+extern OutputFunc g_ut_output;
+
+void ut_output(const QString &);
+void ut_set_output(OutputFunc f);
+
+extern uint g_total_test_count;
+extern uint g_total_fail_count;
+
+typedef const char cchar;
+
+#define		test(b)			_test(b, __FILE__, __LINE__)
+#define		test_equal(e, v)	_test_equal(e, v, __FILE__, __LINE__)
+
+namespace unit_test {
+	extern size_t n_test;			//	トータルテスト回数
+	extern size_t n_failed;			//	トータル失敗回数
+}
+
+struct SFailedInfo
+{
+	QString		m_fileName;
+	int				m_lineNum;
+	QString		m_message;
+public:
+	SFailedInfo(const QString &fileName, int lineNum, QString mess="")
+		: m_fileName(fileName), m_lineNum(lineNum), m_message(mess) {};
+	SFailedInfo(const SFailedInfo &src)
+		: m_fileName(src.m_fileName), m_lineNum(src.m_lineNum), m_message(src.m_message) {};
+	~SFailedInfo() {};
+
+	const SFailedInfo &operator=(const SFailedInfo &src)
+	{
+		m_fileName = src.m_fileName;
+		m_lineNum = src.m_lineNum;
+		m_message = src.m_message;
+		return *this;
+	};
+};
+
+
+class CUnitTest
+{
+	QString	m_unitName;
+	int		m_testCount;		//	テスト回数
+	int		m_failCount;		//	失敗回数
+	//CVOStream *m_out;		//	出力ストリーム
+
+	std::vector<SFailedInfo>	m_logs;
+public:
+	CUnitTest(cchar *unitName /*, CVOStream* = &g_vostream*/);
+	~CUnitTest(void);
+
+public:
+	void _test(bool b, cchar *fileName, int lineNum);
+
+	template<typename T>
+	void _test_equal(const T &exp, const T &v, cchar *fileName, int lineNum)
+	{
+		m_testCount += 1;
+		unit_test::n_test += 1;
+		if( exp == v ) {
+			g_ut_output(".");
+		} else {
+			m_failCount += 1;
+			unit_test::n_failed += 1;
+			g_ut_output("F");
+
+			m_logs.push_back(SFailedInfo(fileName, lineNum));
+		}
+	}
+	void _test_equal(int exp, int v, cchar *fileName = __FILE__, int lineNum = __LINE__)
+	{
+		m_testCount += 1;
+		unit_test::n_test += 1;
+		if( exp == v ) {
+			g_ut_output(".");
+		} else {
+			m_failCount += 1;
+			unit_test::n_failed += 1;
+			g_ut_output("F");
+
+			QString temp = QString("%1 を期待しましたが %2 でした。").arg(exp).arg(v);
+			qDebug() << temp;
+#if 0
+			std::string temp;
+			temp += boost::lexical_cast<std::string>(exp);
+			temp += " を期待しましたが ";
+			temp += boost::lexical_cast<std::string>(v);
+			temp += " でした。";
+#endif
+			m_logs.push_back(SFailedInfo(fileName, lineNum, temp));
+		}
+	}
+	void _test_equal(const QString &exp, const QString &v, cchar *fileName, int lineNum)
+	{
+		m_testCount += 1;
+		unit_test::n_test += 1;
+		if( exp == v ) {
+			g_ut_output(".");
+		} else {
+			m_failCount += 1;
+			unit_test::n_failed += 1;
+			g_ut_output("F");
+
+			QString temp = QString("'%1' を期待しましたが '%2' でした。").arg(exp).arg(v);
+			m_logs.push_back(SFailedInfo(fileName, lineNum, temp));
+		}
+	}
+	void _test_equal(cchar *exp, cchar *v, cchar *fileName, int lineNum)
+	{
+		m_testCount += 1;
+		unit_test::n_test += 1;
+		if( !strcmp(exp, v) ) {
+			g_ut_output(".");
+		} else {
+			m_failCount += 1;
+			unit_test::n_failed += 1;
+			g_ut_output("F");
+
+			QString temp = QString("%1 を期待しましたが %2 でした。").arg(exp).arg(v);
+			m_logs.push_back(SFailedInfo(fileName, lineNum, temp));
+		}
+	}
+
+};
