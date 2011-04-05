414b634a3fd64e51045b71985a00f6f9c8476b9b
vivisuke <vivisuke@3a3fcbb5-9455-4931-b1bf-4cf1dfffbb8a>
2011-04-05 23:36:28 +0000

    git-svn-id: https://svn.osdn.net/svnroot/vivi-50-free/trunk@32 3a3fcbb5-9455-4931-b1bf-4cf1dfffbb8a

--- vivi50/main.cpp
+++ vivi50/main.cpp
@@ -1,3 +1,25 @@
+//----------------------------------------------------------------------
+//
+//			File:			"main.cpp"
+//			Created:		01-Apr-2011
+//			Author:			Nobuhide Tsuda
+//			Description:	
+//
+//----------------------------------------------------------------------
+
+/*
+
+	Copyright (C) 2011 by Nobuhide Tsuda
+
+	本ソースコードは基本的に MIT ライセンスに従う。
+	http://www.opensource.org/licenses/mit-license.php
+	http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license
+
+	ただし、おいらは不自由で使い勝手の悪い GPL が大嫌いなので、
+	GPL ライセンスプロジェクトが本ソースを流用することを禁じる
+
+*/
+
 #include "mainwindow.h"
 #include <QtGui/QApplication>
 #include <QTextCodec>
